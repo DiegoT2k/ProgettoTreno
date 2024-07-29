dba04833604bfa639fe1e048b055c846c07e19f3
DiegoT2k <87203188+DiegoT2k@users.noreply.github.com>
2024-07-29 10:24:33 +0200

    Delete entity LocomotivaItalo

--- Treno/src/main/java/com/corso/model/vagone/impl/LocomotivaItalo.java
+++ Treno/src/main/java/com/corso/model/vagone/impl/LocomotivaItalo.java
@@ -2,11 +2,9 @@ package com.corso.model.vagone.impl;
 
 import com.corso.model.vagone.LocomotivaAbs;
 
-@Entity
+
 public class LocomotivaItalo extends LocomotivaAbs{
 
-	@id
-	@generatedvlaue
-	private int id_vagone
+
 	
 }
