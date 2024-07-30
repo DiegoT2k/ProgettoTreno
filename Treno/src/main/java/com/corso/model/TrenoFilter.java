5e23168b3f26486e06ba8e799f3ab22678afa9b4
DiegoT2k <87203188+DiegoT2k@users.noreply.github.com>
2024-07-30 11:36:37 +0200

    Add TrenoFilter

---
+++ Treno/src/main/java/com/corso/model/TrenoFilter.java
@@ -0,0 +1,25 @@
+package com.corso.model;
+
+public class TrenoFilter {
+
+	int id_utente;
+	
+	private Integer num_biglietti = null;
+
+	public int getId_utente() {
+		return id_utente;
+	}
+
+	public void setId_utente(int id_utente) {
+		this.id_utente = id_utente;
+	}
+
+	public Integer getNum_biglietti() {
+		return num_biglietti;
+	}
+
+	public void setNum_biglietti(Integer num_biglietti) {
+		this.num_biglietti = num_biglietti;
+	}
+	
+}
